1f8d4d3fcd1a6d354ea9e7c683d8f0cd5ae64222
jlpmartinss <jm8244936@gmail.com>
2024-02-20 11:25:18 +0000

    initial commit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,19 @@
-import './menu.css'
-import { BsYoutube, BsInstagram } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import "./menu.css";
+import { BsInstagram, BsGithub } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-    return (
-        <div className='menu'>
-            <a className='social' href='https://www.youtube.com/'>
-                <BsYoutube color='#fff' size={24} />
-            </a>
-            <a className='social' href='https://www.instagram.com/'>
-                <BsInstagram color='#fff' size={24} />
-            </a>
-            <Link className='menu-item' to="/links">
-                My Links
-            </Link>
-        </div>
-    )
+  return (
+    <div className="menu">
+      <a className="social" href="https://github.com/jlpmartinss/">
+        <BsGithub color="#fff" size={24} />
+      </a>
+      <a className="social" href="https://www.instagram.com/jmarti_ns/">
+        <BsInstagram color="#fff" size={24} />
+      </a>
+      <Link className="menu-item" to="/links">
+        My Links
+      </Link>
+    </div>
+  );
 }
